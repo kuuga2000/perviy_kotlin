e0e355738634ad4564235e4257eafc7d667f907b
Kuuga2000 <--edit>
2023-06-05 23:11:37 +0700

    Google Extend classes with new properties and methods

--- google/SingletonObject.kt
+++ google/SingletonObject.kt
@@ -10,15 +10,39 @@ class Quiz {
     val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
     companion object StudentProgress {
         var total: Int = 15
-        var answered: Int = 14
+        var answered: Int = 10
     }
+}
+
+/**
+ * Add an extension property
+ * Note: Extension properties can't store data, so they must be get-only.
+ */
+val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered."
+
+/**
+ * Add an extension function
+ */
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) {
+        print("▓")
+    }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(Quiz.progressText)
 }
 
 fun main() {
     println("${StudentProgressBasic.answered} of ${StudentProgressBasic.total} answered.")
-
     /**
      * object with companion
      */
     println("${Quiz.answered} of ${Quiz.total} answered.")
+
+    println(Quiz.progressText)
+
+    Quiz.printProgressBar()
+
+    print("test"); print("ab")
 }
