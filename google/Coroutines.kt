e3f731f91e56243409374001ff35345f3289b035
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-07-05 17:26:33 +0700

    Coroutines Suspending functions

--- google/Coroutines.kt
+++ google/Coroutines.kt
@@ -1,9 +1,20 @@
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+
 fun main() {
     runBlocking {
         println("Weather forecast")
-        delay(1000)
-        println("Sunny")
+        printForecast()
+        printTemperature()
     }
 }
+
+suspend fun printForecast() {
+    delay(1000)
+    println("Sunny")
+}
+
+suspend fun printTemperature() {
+    delay(1000)
+    println("30\u00b0C")
+} 
