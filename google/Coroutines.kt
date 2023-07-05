431e6e80e1a8424d4e372d28dcb7df5bfaf4cd62
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-07-05 17:50:04 +0700

    Asynchronous code runBlocking()

--- google/Coroutines.kt
+++ google/Coroutines.kt
@@ -1,22 +1,21 @@
+
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlin.system.measureTimeMillis
 
 fun main() {
-    val time = measureTimeMillis {
-        runBlocking {
-            println("Weather forecast")
-            launch {
-                printForecast()
-            }
-            launch {
-                printTemperature()
-            }
+    runBlocking {
+        println("Weather forecast")
+        launch {
+            printForecast()
         }
+        launch {
+            printTemperature()
+        }
+        println("Have a good day!")
     }
-    println("Execution time: ${time / 1000.0} seconds")
 }
+
 
 
 suspend fun printForecast() {
