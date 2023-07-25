9b5bde5d57d66333561fbb65c124c836ba6a9791
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-07-25 18:05:06 +0700

    org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
 }
 
 tasks.test {
