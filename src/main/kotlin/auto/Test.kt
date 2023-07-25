97ca35dd33e8ceccff75528b4eb6d7d5e65aaf15
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-07-25 20:01:32 +0700

    find enum

--- src/main/kotlin/auto/Test.kt
+++ src/main/kotlin/auto/Test.kt
@@ -1,12 +1,21 @@
-
-
 enum class CupcakeScreen(val title: String = "1") {
     Start(title = "Asd"),
     Flavor(title = "x"),
     Pickup(title = "z"),
     Summary(title = "c")
 }
+enum class Color(val colorName: String, val rgb: String) {
+    RED("Red", "#FF0000"),
+    ORANGE(
+        "Orange",
+        "#FF7F00"
+    ),
+    YELLOW("Yellow", "#FFFF00")
+}
+
+fun findByRgb(rgb: String): Color? = Color.entries.find { it.rgb == rgb }
 
 fun main() {
-    println(CupcakeScreen.Start.name)
+    val a = findByRgb("#FF0000");
+    println(Color.entries.find{ it.rgb == "#FF0000"} )
 }
